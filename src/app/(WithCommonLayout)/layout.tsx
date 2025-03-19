5168354ade11536fefb770c2230fde23fe04ce52
alamshuvo <iftakharalamshuvo11@gmail.com>
2025-03-19 23:30:02 +0600

    some code added

---
+++ src/app/(WithCommonLayout)/layout.tsx
@@ -0,0 +1,11 @@
+
+
+const CommonLayout = ({children}:{children:React.ReactNode}) => {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+};
+
+export default CommonLayout;
---
+++ src/app/(WithCommonLayout)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+
+const HomePage = () => {
+  return (
+    <div>
+      <Button className="bg-red-500">Shad Cn Button </Button>
+      <p>Welcome to nextMart Home page</p>
+    </div>
+  );
+};
+
+export default HomePage;
